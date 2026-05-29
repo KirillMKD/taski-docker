4612648e8b9764762378cc88120d48b201ea13ac
Kirill M <KiryaMKD@yandex.ru>
2026-05-29 18:51:11 +0300

    Fix flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
+
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -104,8 +104,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
